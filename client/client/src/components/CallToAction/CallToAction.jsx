e2d2ad56498f5a1e9d939b05ef316b6dfa7a2281
Hajerabdeen <154739761+Hajerabdeen@users.noreply.github.com>
2024-11-09 22:39:05 +0200

    Delete client/client directory

--- client/client/src/components/CallToAction/CallToAction.jsx
+++
@@ -1,5 +0,0 @@
-function CallToAction() {
-    return <h1>CallToAction</h1>;
-  }
-  
-  export default CallToAction;
--- client/client/src/components/Features/FeatureCard.jsx
+++
@@ -1,5 +0,0 @@
-function FeatureCard() {
-    return <h1>FeatureCard</h1>;
-  }
-  
-  export default FeatureCard;
